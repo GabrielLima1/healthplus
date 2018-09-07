0b52ae665c355e816b16344ecd822ce980ffe5aa
GabrielLima1 <gabriellimatrajano@hotmail.com>
2018-09-07 15:23:14 -0300

    adicionando métodos na camada DAO do FormularioContato

--- src/br/com/fiap/dao/FormularioContatoDAO.java
+++ src/br/com/fiap/dao/FormularioContatoDAO.java
@@ -19,6 +19,7 @@ public class FormularioContatoDAO {
 		con = Conexao.conectar();
 	}
 	
+	
 	public String gravar(FormularioContato form) throws Exception{
 		stmt = con.prepareStatement("INSERT INTO FORMULARIO_CONTATO (cod_contato,nome_contato,email_contato,mensagem_contato,tipo_contato) VALUES(?,?,?,?,?)");
 		stmt.setInt(1, form.getCodigo());
@@ -44,6 +45,72 @@ public class FormularioContatoDAO {
 		
 		return form;
 		
+	}
+	
+	// metodo para consultar pelo tipo
+	public List<FormularioContato> consultarPorTipo(String tipo) throws Exception {
+		List<FormularioContato> form = new ArrayList<FormularioContato>();
+		stmt = con.prepareStatement("SELECT * FROM FORMULARIO_CONTATO WHERE tipo_contato LIKE ?");
+		stmt.setString(1, "%"+tipo+"%");
+		rs = stmt.executeQuery();
+		while(rs.next()) {
+			form.add(new FormularioContato(rs.getInt("cod_contato"),
+										   rs.getString("nome_contato"),
+										   rs.getString("email_contato"),
+										   rs.getString("mensagem_contato"),
+										   rs.getString("tipo_contato")));
+		}
+		
+		return form;
+		
+	}
+	
+	// metodo para consultar pelo email
+	public List<FormularioContato> consultarPorEmail(String email) throws Exception {
+		List<FormularioContato> form = new ArrayList<FormularioContato>();
+		stmt = con.prepareStatement("SELECT * FROM FORMULARIO_CONTATO WHERE email_contato LIKE ?");
+		stmt.setString(1, "%"+email+"%");
+		rs = stmt.executeQuery();
+		while(rs.next()) {
+			form.add(new FormularioContato(rs.getInt("cod_contato"),
+										   rs.getString("nome_contato"),
+										   rs.getString("email_contato"),
+										   rs.getString("mensagem_contato"),
+										   rs.getString("tipo_contato")));
+		}
+		
+		return form;
+		
+	}
+	
+	
+	// metodo para consultar pela mensagem
+	public List<FormularioContato> consultarPorMensagem(String mensagem) throws Exception {
+		List<FormularioContato> form = new ArrayList<FormularioContato>();
+		stmt = con.prepareStatement("SELECT * FROM FORMULARIO_CONTATO WHERE mensagem_contato LIKE ?");
+		stmt.setString(1, "%"+mensagem+"%");
+		rs = stmt.executeQuery();
+		while(rs.next()) {
+			form.add(new FormularioContato(rs.getInt("cod_contato"),
+										   rs.getString("nome_contato"),
+										   rs.getString("email_contato"),
+										   rs.getString("mensagem_contato"),
+										   rs.getString("tipo_contato")));
+		}
+		
+		return form;
+		
+	}
+	
+	public int apagar(int numero) throws Exception{
+		stmt = con.prepareStatement("DELETE FROM FORMULARIO_CONTATO WHERE cod_contato = ?");
+		stmt.setInt(1, numero);
+		return stmt.executeUpdate();
+	}
+	
+	// metodo para fechar conexão com o banco
+	public void fechar() throws Exception{
+		con.close();
 	}
 	
 	
