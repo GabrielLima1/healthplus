51bd59f8621fa849f30fefe32d0227fe65ac4574
GabrielLima1 <gabriellimatrajano@hotmail.com>
2018-09-21 01:20:59 -0300

    Novos BO

--- src/br/com/fiap/bo/EspecialidadeBO.java
+++ src/br/com/fiap/bo/EspecialidadeBO.java
@@ -4,43 +4,108 @@ import br.com.fiap.beans.Especialidade;
 import br.com.fiap.dao.EspecialidadeDAO;
 
 public class EspecialidadeBO {
+	
+	public static String verificacaoInicial(Especialidade esp)throws Exception{
+		
+		if(esp.getNome() ==  null || esp.getNome() ==  "" || esp.getNome().length()>60) {
+			return "Nome Especialidade Inválido!";
+		}
+		
+		return "OK";
+	}
 
 	public static String cadastroEspecialidade(Especialidade esp)throws Exception{
 		
-		if(esp.getNome().length()== 0 || esp.getNome().length()>60) {
-			return "Nome Especialidade Inválido!";
-		}
-				
-		EspecialidadeDAO dao = new EspecialidadeDAO();
-		Especialidade e = dao.consultarPorCodigo(esp.getCodigo());
-		if(e.getCodigo()>0) {
-			dao.fechar();
-			return "Código Especialidade já Existe!";
-		}
+		String resposta = verificacaoInicial(esp);
 		
-		e = dao.consultarPorNome(esp.getNome());
-		if(!e.getNome().equals(null)) {
-			return "Nome Especialidade já Existe!";
+		if(resposta.equals("OK")){
+				
+				EspecialidadeDAO dao = new EspecialidadeDAO();
+				Especialidade e = dao.consultarPorCodigo(esp.getCodigo());
+				
+				if(e.getCodigo()>0) {
+					dao.fechar();
+					return "Código Especialidade já Existe!";
+				}
+
+				e = dao.consultarPorNome(esp.getNome());
+				if(e.getNome() != null) {
+					return "Nome Especialidade já Existe!";
+				}
+				
+				resposta = "PASSOU";
 		}
 		
 		esp.setNome(esp.getNome().toUpperCase());
 		
-		String resposta = dao.gravar(esp);
-		dao.fechar();
+		EspecialidadeDAO dao = new EspecialidadeDAO();
+		
+		if(resposta.equals("PASSOU")) {
+			resposta = dao.gravar(esp);
+			dao.fechar();
+			
+		}
 		return resposta;
 	}
 	
 	public static String atualizarEspecialidade(Especialidade esp)throws Exception {
+		
 		String resposta = "";
+		
+		if(esp.getCodigo() == 0) {
+			return "Código Usúario Inválido!";
+		}
+		
 		EspecialidadeDAO dao = new EspecialidadeDAO();
 		Especialidade e = dao.consultarPorCodigo(esp.getCodigo());
+		
 		if(e.getCodigo()>0) {
-			resposta = dao.atualizaEspecialidade(esp);
+			resposta = verificacaoInicial(esp);
+			
+			if(resposta.equals("OK")) {
+				resposta = dao.atualizaEspecialidade(esp);
+				dao.fechar();
+			}		
+			
 		}else {
-			return "Especialidade não existe!";
+			return "Código Inválido!";
 		}
-		dao.fechar();
 		return resposta;
+				
 	}
 	
+	
+	public static Especialidade pesquisaPorCodigoEspecialidade(int codigo)throws Exception{
+		if(codigo < 1) {
+			return new Especialidade();
+		}
+		
+		EspecialidadeDAO dao = new EspecialidadeDAO();
+		Especialidade esp = dao.consultarPorCodigo(codigo);
+		dao.fechar();
+		return esp;
+	}
+	
+	public static Especialidade pesquisaPorNomeEspecialidade(String nome)throws Exception{
+		if(nome.length() < 1) {
+			return new Especialidade();
+		}
+		
+		EspecialidadeDAO dao = new EspecialidadeDAO();
+		Especialidade espe = dao.consultarPorNome(nome.toUpperCase());
+		dao.fechar();
+		return espe;
+		}
+	
+	public static int excluirPorCodigoEspecialidade(int codigo)throws Exception{
+		if(codigo < 1) {
+			return 0;
+		}
+		
+		EspecialidadeDAO dao = new EspecialidadeDAO();
+		int x = dao.apagar(codigo);
+		dao.fechar();
+		return x;
+	}
 }
+
--- src/br/com/fiap/teste/Teste.java
+++ src/br/com/fiap/teste/Teste.java
@@ -1,24 +1,29 @@
 package br.com.fiap.teste;
 
-import java.util.ArrayList;
-import java.util.List;
+import br.com.fiap.beans.Especialidade;
+import br.com.fiap.bo.EspecialidadeBO;
+import br.com.fiap.excecao.Excecao;
 
-import br.com.fiap.beans.Clinica;
-import br.com.fiap.dao.ClinicaDAO;
+
 
 public class Teste {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try {
-			ClinicaDAO dao = new ClinicaDAO();
-			String resultado = new String();
-			Clinica c = new Clinica();
-			dao.gravar(c.getCodigo(), c.getNome(), c.getEmail(), c.getCnpj(), c.getSite(), c.getHorario(), 'N', 0, c.getTelefone(),  2, 3);
+			EspecialidadeBO bo = new EspecialidadeBO();
+			Especialidade esp = new Especialidade();
+			
+			esp.setCodigo(5);
+			//esp.setNome("Vamos");
+			
+		 Especialidade espe =  bo.excluirPorCodigoEspecialidade(getCodigo());
+			System.out.println(espe.getCodigo());
 		
 			
 		}catch(Exception ex) {
 			ex.printStackTrace();
+			System.out.println(Excecao.tratarExcecao(ex));
 		}
 	}
 
